69c022958e85e840bc55c2746e6cbaca6777773b
Cornelius Weidmann <cornelius@kycosoftware.com>
2016-05-15 21:20:47 +0200

    Update

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,14 +1,14 @@
-var kmv = kmv || false;
+var kmvyo = kmvyo || false;
 
 (function() {
-  if (!kmv) {
-    var a = document.createElement('script');
-    a.setAttribute('id', 'kmv');
-    a.setAttribute('src', 'https://corhero.github.io/kmv/init.js');
+  if (!kmvyo) {
+    var scripto = document.createElement('script');
+    scripto.setAttribute('id', 'kmv');
+    scripto.setAttribute('src', 'https://corhero.github.io/kmv/init.js');
     document.body.appendChild(a);
-    kmv = true;
+    kmvyo = true;
   } else {
-    var b = document.getElementById('kmviframe');
-    a.setAttribute('class', '');
+    var iframo = document.getElementById('kmviframe');
+    iframo.setAttribute('class', '');
   }
 })();
